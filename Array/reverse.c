9cbb758b123fb9e35fc7a2fe69a6d92910aebea1
ayush24401 <ayushshende8504@gmail.com>
2022-04-29 22:15:40 +0530

    reverseArray Program

--- Array/reverse.c
+++ Array/reverse.c
@@ -1,4 +1,41 @@
-void main()
+#include<stdio.h>
+#include<stdlib.h>
+
+void printArr(int arr[],int size)
 {
-    printf("Hello World");
+    for(int i=0;i<size;i++)
+    {
+        printf("%d  ",arr[i]);
+    }
+}
+
+void swap(int arr[],int i,int j)
+{
+    int temp;
+    temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+
+void reversearr(int arr[],int size)
+{
+    for(int i=0;i<size/2;i++)
+    {
+        int j = size-i-1;
+        swap(arr,i,j);
+    }
+}
+
+int main()
+{
+    // printf("Hello World");
+    int arr[]={1,2,3,4,5,6};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    // printf("%d",size);
+    printf("Before reversing\n");
+    printArr(arr,size);
+    reversearr(arr,size);
+    printf("\nAfter reversing\n");
+    printArr(arr,size);
+    return 0;
 }
