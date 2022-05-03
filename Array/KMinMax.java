2e6079c2c3cfb55d7b7106d2a192dcedd03cc36e
chavanmahesh06 <maheshvchavan2002@gmail.com>
2022-05-03 21:53:49 +0530

    initial

---
+++ Array/KMinMax.java
@@ -0,0 +1,58 @@
+package Array;
+
+public class KMinMax 
+{
+    static void getMin(int arr[], int size, int k)
+    {
+        int temp;
+        int i,j;
+        for (i=0;i<size;i++)
+        {
+            for(j= i+1;j<size;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        // for (i = 0; i < arr.length; i++) {     
+        //     System.out.print(arr[i] + " ");    
+        // }    
+
+        System.out.printf("\n The %d th minimum element is %d \n", k,arr[k-1]);
+    }
+
+    static void getMax(int arr[], int size, int k)
+    {
+        int temp;
+        int i,j;
+        for (i=0;i<size;i++)
+        {
+            for(j= i+1;j<size;j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        // for (i = 0; i < arr.length; i++) {     
+        //     System.out.print(arr[i] + " ");    
+        // }    
+        System.out.printf("\n The %d th maximum element is %d \n", k,arr[k-1]);
+    }
+
+    public static void main(String args[])
+    {
+        int arr[]={3,4,1,2,6,5};
+        int size= 6;
+        int k;
+        getMax(arr, size, 3);
+        getMin(arr, size, 2);
+    }
+}
